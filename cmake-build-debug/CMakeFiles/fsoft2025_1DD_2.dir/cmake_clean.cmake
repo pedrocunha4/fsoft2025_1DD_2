0d3a9c69ac631cbc2bc7e065a6f9481f65a66a5c
pedrocunha4 <1231690@isep.ipp.pt>
2025-04-01 16:48:23 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/fsoft2025_1DD_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fsoft2025_1DD_2.dir/main.cpp.obj"
+  "CMakeFiles/fsoft2025_1DD_2.dir/main.cpp.obj.d"
+  "fsoft2025_1DD_2.exe"
+  "fsoft2025_1DD_2.exe.manifest"
+  "fsoft2025_1DD_2.pdb"
+  "libfsoft2025_1DD_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fsoft2025_1DD_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/fsoft2025_1DD_2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for fsoft2025_1DD_2.
+# This may be replaced when dependencies are built.
