5c0d7f115bf0d3fe58f81d24d4fc6c374f403219
pedrocunha4 <1231690@isep.ipp.pt>
2025-06-04 00:06:16 +0100

    pequeno ajuste

--- Project/sources/controllers/Controller.cpp
+++ Project/sources/controllers/Controller.cpp
@@ -501,7 +501,7 @@ void Controller::manageClientsMenu() {
                 deleteClientByEmail();
             break;
             case 3:
-                completeClientOrder();
+                // completeClientOrder(); NOT IMPLEMENTED
             break;
             case 0:
                 std::cout << "Returning to Manager Menu...\n";
